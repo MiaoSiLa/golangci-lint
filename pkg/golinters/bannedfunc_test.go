d2720cb5484199ed5fb21209af690985b5bbf660
seige <486315944@qq.com>
2020-11-11 16:13:27 +0800

    test

--- pkg/golinters/bannedfunc_test.go
+++ pkg/golinters/bannedfunc_test.go
@@ -11,26 +11,26 @@ func TestDecodeFile(t *testing.T) {
 	assert := assert.New(t)
 	require := require.New(t)
 	var b = []byte("linters-settings:\n  bannedfunc:\n    (time).Now: \"不能使用 time.Now() 请使用 MiaoSiLa/missevan-go/util 下 TimeNow()\"\n    (github.com/Missevan/missevan-go/util/time).TimeNow: \"xxxx\"")
-	require.NotPanics(func() {
-		setting := decodeFile(b)
-		require.NotNil(setting.LinterSettings)
-		val, ok := setting.LinterSettings.Funcs["(time).Now"]
-		assert.True(ok)
-		assert.Equal("不能使用 time.Now() 请使用 MiaoSiLa/missevan-go/util 下 TimeNow()", val)
-		val, ok = setting.LinterSettings.Funcs["(github.com/Missevan/missevan-go/util/time).TimeNow"]
-		assert.True(ok)
-		assert.Equal("xxxx", val)
-	})
+	var setting configSetting
+	require.NotPanics(func() { setting = decodeFile(b) })
+	require.NotNil(setting.LinterSettings)
+	val, ok := setting.LinterSettings.Funcs["(time).Now"]
+	assert.True(ok)
+	assert.Equal("不能使用 time.Now() 请使用 MiaoSiLa/missevan-go/util 下 TimeNow()", val)
+	val, ok = setting.LinterSettings.Funcs["(github.com/Missevan/missevan-go/util/time).TimeNow"]
+	assert.True(ok)
+	assert.Equal("xxxx", val)
 }
 
 func TestConfigToConfigMap(t *testing.T) {
 	assert := assert.New(t)
 	require := require.New(t)
-	var m = make(map[string]string)
-	m["(time).Now"] = "不能使用 time.Now() 请使用 MiaoSiLa/missevan-go/util 下 TimeNow()"
-	m["(github.com/Missevan/missevan-go/util/time).TimeNow"] = "xxxx"
-	m["()."] = "().情况"
-	m[")."] = ").情况"
+	var m = map[string]string{
+		"(time).Now": "不能使用 time.Now() 请使用 MiaoSiLa/missevan-go/util 下 TimeNow()",
+		"(github.com/Missevan/missevan-go/util/time).TimeNow": "xxxx",
+		"().": "(). 情况",
+		").":  "). 情况",
+	}
 	s := configSetting{LinterSettings: BandFunc{Funcs: m}}
 	setting := configToConfigMap(s)
 	require.NotNil(setting["time"])
